f32f98bdd157a9a9ccc43b7b14b8b67ae450eb84
Yutaro Sakamoto <mail@yutaro-sakamoto.com>
2025-10-08 10:07:54 +0000

    add: open_interval

--- lean-proof/LeanProof/Basic.lean
+++ lean-proof/LeanProof/Basic.lean
@@ -1,5 +1,14 @@
 import Mathlib.Tactic.NormNum
+import Mathlib.Topology.Basic
+import Mathlib.Data.Set.Basic
+import Mathlib.Data.Real.Basic
+import Mathlib.Tactic.Linarith
+import Mathlib.Data.Real.Archimedean
+import Mathlib.Topology.MetricSpace.Basic
 
 def hello := "world"
 
 example : 1 + 1 = 2 := by norm_num
+
+-- 実数の開区間を定義
+def open_interval (a b : ℝ) : Set ℝ := {x | a < x ∧ x < b}
